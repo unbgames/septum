83df0c235d3f4d7bfe77c5c1aa47756e3d541895
eduardoforca <edusousa1996@hotmail.com>
2018-04-05 23:48:40 -0300

    Added various geometric fucntions

--- include/Vec2.h
+++ include/Vec2.h
@@ -5,5 +5,56 @@
  */
 class Vec2 {
 	public:
+		Vec2 ();
+		/**
+		 * Vec2 constructor
+		 * @param x position on x-axis
+		 * @param y position on y-axis
+		 */
+		Vec2 (float x, float y);
+		/**
+		 * Returns the length(magnitude) of a vector
+		 * @return vector length
+		 */
+		float GetLength () const;
+		/**
+		 * Return the unit vector of a vector
+		 *
+		 * An unit vector is a vector which has the same angle as nother vector but magnitude of 1
+		 * @return unit vector
+		 */
+		Vec2 GetUnit () const;
+
+		Vec2 operator+ (const Vec2 other) const;
+		Vec2& operator+= (const Vec2 other);
+		Vec2 operator- (const Vec2 other) const;
+		Vec2& operator-= (const Vec2 other);
+		Vec2 operator* (const int number) const;
+		Vec2& operator*= (const int number) const;
+
+		/**
+		 * Returns the distance between two points
+		 * @param other the other point
+		 * @return distance
+		 */
+		float GetDistance (const Vec2 other) const;
+		/**
+		 * Returns the angle of a Vector
+		 * @return angle of the vector
+		 */
+		float GetAngle () const;
+		/**
+		 * Return the angle of a vector between 2 points
+		 * @param other the other point
+		 * @return angle of the vector
+		 */
+		float GetAngle (const Vec2 other) const;
+		/**
+		 * Returns the rotated vector
+		 * @param angle angle of rotation
+		 * @return rotated vector
+		 */
+		Vec2 GetRotated (float angle) const;
+
 		float x, y;
 };
---
+++ src/Rect.cpp
@@ -0,0 +1,33 @@
+#include "Rect.h"
+
+Rect::Rect () :
+		x(0), y(0), w(0), h(0) {
+}
+
+Rect::Rect (float x, float y, float w, float h) :
+		x(x), y(y), w(w), h(h) {
+	
+}
+
+bool Rect::Contains (Vec2 point) const {
+	return (point.x >= x && point.x <= x + w)
+			&& (point.y >= h && point.y <= y + h);
+}
+
+Vec2 Rect::GetCenter () const {
+	return Vec2(x + w / 2, y + h / 2);
+}
+
+float Rect::GetDistance (Rect other) const {
+	return GetCenter().GetDistance(other.GetCenter());
+}
+
+Rect Rect::operator + (const Vec2 vec) const {
+	return Rect(x, y, w + vec.x, h + vec.y);
+}
+
+Rect& Rect::operator += (const Vec2 vec) {
+	w += vec.x;
+	h += vec.y;
+	return *this;
+}
