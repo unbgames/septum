03bbaa7d686d3feeb7e9bc98b641502becaa391f
Eduardo Força <sousa@eduqc.com>
2018-05-20 17:42:11 -0300

    Correct recoverd StageState

--- include/StageState.h
+++ include/StageState.h
@@ -1,13 +1,8 @@
 #pragma once
 #include "GameObject.h"
 #include "Music.h"
-#include <vector>
 #include "State.h"
-#include <memory>
 
-using std::vector;
-using std::shared_ptr;
-using std::weak_ptr;
 
 /**
  * Class of game current state
@@ -16,18 +11,14 @@ class StageState: public State {
 	public:
 		StageState ();
 		~StageState ();
-		/**
-		 * Returns whether there was a close command to the game
-		 * @return if the close command was issued by the game
-		 */
-		bool QuitRequested () const;
+
 		/**
 		 * Loads game assets
 		 */
 		void LoadAssets ();
 		/**
 		 * Updates game state
-		 * @param dt
+		 * @param dt time interval since last update
 		 */
 		void Update (float dt);
 		/**
@@ -35,29 +26,11 @@ class StageState: public State {
 		 */
 		void Render ();
 
-		/**
-		 * Starts all objects of the StageState
-		 */
 		void Start ();
-
-		/**
-		 * Adds an object to the StageState
-		 * @param go object to be added
-		 * @return weak pointer reference to the game object
-		 */
-		weak_ptr<GameObject> AddObject (GameObject* go);
-
-		/**
-		 * Returns a weak pointer to the passed GameObject
-		 * @param go GameObject
-		 * @return weak pointer
-		 */
-		weak_ptr<GameObject> GetObjectPtr (GameObject* go);
+		void Pause ();
+		void Resume ();
 	private:
 		Music music;
 		GameObject map;
 		GameObject bg;
-		bool quitRequested;
-		bool started;
-		vector<shared_ptr<GameObject>> objectArray;
 };
